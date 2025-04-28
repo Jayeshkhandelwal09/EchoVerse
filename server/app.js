9f54839f031f50fcba819ea161b42fb5c086e3f0
jayeshxira <jayesh.xira@gmail.com>
2025-04-28 19:38:33 +0530

    Cors validation for local

--- server/app.js
+++ server/app.js
@@ -8,7 +8,10 @@ const entryRoutes = require('./routes/entryRoutes');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true, 
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
